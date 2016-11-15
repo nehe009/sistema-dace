7b97b79f2443423b49427c112ac6ff1c0d632af8
nehe009 <nehe009@gmail.com>
2016-11-15 19:21:52 -0400

    cambio de logo en header

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,2 @@
 <br>
-<img src="images/logo01.png" alt="logo01" class="img-responsive hidden-xs">
-<img src="images/logo02.png" alt="logo02" class="img-responsive hidden-xs"> 
+<img src="images/logo.png" alt="logo01" class="img-responsive hidden-xs">
